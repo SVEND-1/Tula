fe04030820c51b816500a729e02028178b9c4214
SVEND-1 <s5090@inbox.ru>
2026-04-17 21:02:22 +0300

    init

---
+++ src/test/java/org/example/tula/TulaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
